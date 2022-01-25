7c738b798fec0fa42d9efe3d21f1b86bddcf188d
Yannis BOUGOURZI <kaiva.kono@gmail.com>
2022-01-25 12:00:10 +0100

    exercice 9

--- src/app/statistique.service.ts
+++ src/app/statistique.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Statistique } from 'src/models/types';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatistiqueService {
   public tabSta!: Statistique[]
-  constructor() { }
+  stats!: Statistique[];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  recupStat() {
+    lastValueFrom(this.http.get<Statistique[]>("https://stats.naminilamy.fr/")).then(
+      res => this.stats = res);
+
+  }
 }
