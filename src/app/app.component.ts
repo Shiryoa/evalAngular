acb6569c6a3ebe45360059c53335ccb1b0818f8b
Yannis BOUGOURZI <kaiva.kono@gmail.com>
2022-01-25 10:35:48 +0100

    exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Statistique } from 'src/models/types';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eval';
+  sta1: Statistique = new Statistique("15dsrf5ezf4e", "Demographie en Espagne", "47M")
+  sta2: Statistique = new Statistique("5ez5d4fe8e5f", "Demographie en Italie", "60M")
 }
